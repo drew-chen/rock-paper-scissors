6123ef125df5145236185fabd4f54fac414e35bd
chen <chen_andrew@berkeley.edu>
2020-01-01 15:41:46 -0800

    Create function for playing one round

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
+/**
+ * Play one round of rock paper scissors.
+ * 
+ * @param {number} playerMove A move ranging from 1 to 3 
+ *                            for rock, paper, or scissors.
+ */
 function round(playerMove){
-    let computerMove = randomMove();
+    let result = winner(playerMove, randomMove());
+    if (result === 0) {
+        ++numTies;
+        return "tie";
+    } else if (result === 1) {
+        ++playerWins; 
+        return "You win!";
+    } else if (result === 2) {
+        ++computerWins;
+        return "You lose :(";
+    }
 }
+/**
+ * Returns a random number between 1 and 3. 
+ * This represents a move by the computer.
+ */
 function randomMove() {
     return Math.floor(Math.random()*2) + 1;
 }
-/* 
-    Returns 1 if the winning move is by firstPlayerMove and returns
-*/
+
 /**
  * Returns 0 if there is a tie, 1 if the winning move is by firstPlayerMove 
  * and returns 2 otherwise.
@@ -35,7 +53,7 @@ function winner(firstPlayerMove, secondPlayerMove) {
         return 2;
     }
 }
-let tieCount = 0;
+let numTies = 0;
 let playerWins = 0;
 let computerWins = 0;
 const ROCK = 1;
