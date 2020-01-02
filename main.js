6ea82eb5b7bf03a1ed03ff5c5feef97003411b3f
chen <chen_andrew@berkeley.edu>
2020-01-01 22:04:49 -0800

    Display all game results and fix random move to include scissor move

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ function round(playerMove){
  * This represents a move by the computer.
  */
 function randomMove() {
-    return Math.floor(Math.random()*2) + 1;
+    return Math.round(Math.random()*3) + 1;
 }
 /**
  * Returns the string representation of 1, 2, or 3.
@@ -83,21 +83,29 @@ function winner(firstPlayerMove, secondPlayerMove) {
         return 2;
     }
 }
+function updateCountDisplay() {
+    PLAYER_WIN_COUNT_DISPLAY.innerText = playerWins;
+    COMPUTER_WIN_COUNT_DISPLAY.innerText = computerWins;
+    TIE_COUNT_DISPLAY.innerText = numTies;
+}
 /**
  * Start the game from the begining and erase progress.
  */
 function restart() {
     started = true;
     numTies = playerWins = computerWins = 0;
+    updateCountDisplay();
 }
 ROCK_BUTTON.onclick = function() {
-    round(ROCK);
-    
+    round(ROCK);    
+    updateCountDisplay();
 }
 PAPER_BUTTON.onclick = function() {
     round(PAPER);
+    updateCountDisplay();
 }
 SCISSORS_BUTTON.onclick = function() {
     round(SCISSORS);
+    updateCountDisplay;
 }
 
